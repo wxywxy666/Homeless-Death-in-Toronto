2283d7775502391a732cbaef13320c08e6fe5fb7
wxywxy666 <wxy1700774887@outlook.com>
2024-01-21 21:15:59 +0000

    1

---
+++ scripts/00-simulate_data.R
@@ -0,0 +1,33 @@
+install.packages("knitr")
+install.packages("janitor")
+install.packages("lubridate")
+install.packages("opendatatoronto")
+install.packages("tidyverse")
+library(knitr)
+library(janitor)
+library(lubridate)
+library(opendatatoronto)
+library(tidyverse)
+
+#### Simulate ####
+set.seed(853)
+
+simulated_occupancy_data <-
+  tibble(
+    date = rep(x = as.Date("2021-01-01") + c(0:364), times = 3),
+    # Based on Eddelbuettel: https://stackoverflow.com/a/21502386
+    shelter = c(
+      rep(x = "Shelter 1", times = 365),
+      rep(x = "Shelter 2", times = 365),
+      rep(x = "Shelter 3", times = 365)
+    ),
+    number_occupied =
+      rpois(
+        n = 365 * 3,
+        lambda = 30
+      ) # Draw 1,095 times from the Poisson distribution
+  )
+
+head(simulated_occupancy_data)
+
+citation()
---
+++ scripts/01-download_data.R
@@ -0,0 +1,21 @@
+library(knitr)
+library(janitor)
+library(lubridate)
+library(opendatatoronto)
+library(tidyverse)
+#### Acquire ####
+toronto_shelters <-
+  # Each package is associated with a unique id  found in the "For 
+  # Developers" tab of the relevant page from Open Data Toronto
+  # https://open.toronto.ca/dataset/daily-shelter-overnight-service-occupancy-capacity/
+  list_package_resources("21c83b32-d5a8-4106-a54f-010dbe49f6f2") |>
+  # Within that package, we are interested in the 2021 dataset
+  filter(name == 
+           "daily-shelter-overnight-service-occupancy-capacity-2021.csv") |>
+  # Having reduced the dataset to one row we can get the resource
+  get_resource()
+
+write_csv(
+  x = toronto_shelters,
+  file = "unedited_data.csv"
+)
